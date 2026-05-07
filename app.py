1ee13bcd14d7e8c637284ae8115d8c4ed1e6ca1a
Amani Selmene <151787973+amani-selmane@users.noreply.github.com>
2026-05-07 16:26:58 +0200

    Refactor Streamlit layout and iframe settings
    Removed caching styles and updated iframe dimensions.

--- app.py
+++ app.py
@@ -7,23 +7,17 @@ st.set_page_config(
     layout="wide"
 )
 
-# Cache la barre Streamlit et les marges
-st.markdown("""
-    <style>
-        .block-container {
-            padding: 0 !important;
-            margin: 0 !important;
-        }
-        header {display: none !important;}
-        footer {display: none !important;}
-    </style>
-""", unsafe_allow_html=True)
+st.title("📊 Dashboard Multi_Company Analytics")
+st.markdown("---")
 
 EMBED_URL = "https://app.powerbi.com/reportEmbed?reportId=efaf934a-f411-4ac5-b0fc-7db259900b50&autoAuth=true&ctid=a62ee7c4-ed2d-4991-b4b8-3120e8333e11"
 
 components.iframe(
     src=EMBED_URL,
-    width=1600,
-    height=900,
+    width=None,
+    height=820,
     scrolling=True
 )
+
+st.markdown("---")
+st.caption("Données provenant de Power BI Service")
