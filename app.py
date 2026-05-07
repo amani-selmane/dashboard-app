8a10710217bc41ed906401271deac8a9db613677
Amani Selmene <151787973+amani-selmane@users.noreply.github.com>
2026-05-07 16:24:37 +0200

    Modify Streamlit layout and iframe dimensions
    Updated Streamlit layout and embedded Power BI report dimensions.

--- app.py
+++ app.py
@@ -7,17 +7,23 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("📊 Dashboard Multi_Company Analytics")
-st.markdown("---")
+# Cache la barre Streamlit et les marges
+st.markdown("""
+    <style>
+        .block-container {
+            padding: 0 !important;
+            margin: 0 !important;
+        }
+        header {display: none !important;}
+        footer {display: none !important;}
+    </style>
+""", unsafe_allow_html=True)
 
 EMBED_URL = "https://app.powerbi.com/reportEmbed?reportId=efaf934a-f411-4ac5-b0fc-7db259900b50&autoAuth=true&ctid=a62ee7c4-ed2d-4991-b4b8-3120e8333e11"
 
 components.iframe(
     src=EMBED_URL,
-    width=None,
-    height=820,
+    width=1600,
+    height=900,
     scrolling=True
 )
-
-st.markdown("---")
-st.caption("Données provenant de Power BI Service")
