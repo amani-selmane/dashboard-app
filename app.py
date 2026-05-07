c5e336f055d78f8473e2fdfef57835781e32a0fa
Amani Selmene <151787973+amani-selmane@users.noreply.github.com>
2026-05-07 16:38:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,6 @@ st.set_page_config(
     layout="wide"
 )
 
-st.markdown("""
-    <style>
-        .block-container {
-            padding: 1rem 1rem 0rem 1rem !important;
-            max-width: 100% !important;
-        }
-        section[data-testid="stMain"] {
-            overflow-x: hidden;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
 st.title("📊 Dashboard Multi_Company Analytics")
 st.markdown("---")
 
@@ -26,9 +14,9 @@ EMBED_URL = "https://app.powerbi.com/reportEmbed?reportId=efaf934a-f411-4ac5-b0f
 
 components.iframe(
     src=EMBED_URL,
-    width=1400,
+    width=None,
     height=820,
-    scrolling=False
+    scrolling=True
 )
 
 st.markdown("---")
